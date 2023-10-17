cafedac1d02ba10cd1b439ad10a3cfdec4ae08fc
Arif <arif@Arifs-MacBook-Pro.local>
2023-10-17 14:03:52 +0530

    Introduce constants related to different options for creating a trip

---
+++ src/utils/constants.tsx
@@ -0,0 +1,49 @@
+// Places
+export const destinationOptions: string[] = [
+  'India',
+  'Germany',
+  'United Kingdom',
+  'United States',
+  'Australia',
+];
+
+// Interests
+export const interestOptions: string[] = [
+  'Adventures & Outdoors',
+  'Heritage & Culture',
+  'Nature & Landscapes',
+  'Wildlife & Safaris',
+  'Wine & Food',
+  'Beaches',
+];
+
+// Travel Group Size
+export const travelSizeOptions: string[] = [
+  '1 traveler',
+  '2 travelers',
+  '3 travelers',
+  '4 travelers',
+  '5 travelers',
+  '6 travelers',
+  '6+ travelers',
+];
+
+// Budget Options
+export const budgetOptions: string[] = [
+  '$4000 - $5000',
+  '$5000 - $6000',
+  '$6000 - $7000',
+  '$7000 - $8000',
+  '$8000 - $10000',
+  '$10000+',
+];
+
+// Travel Dates
+export const whenOptions: string[] = ['October 2023', 'November 2023'];
+
+// Travel Stages
+export const stagesOptions: string[] = [
+  'Still dreaming / researching',
+  'Definitely traveling, need destination expertise',
+  'I want to book a trip',
+];
